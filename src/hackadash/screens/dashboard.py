909c5347a226df4408a1d4b20429873ab1113a84
Chicken502 <bcnelson100@gmail.com>
2026-09-07 13:34:10 -0400

    Changed API URL to get more data from one call

--- src/hackadash/screens/dashboard.py
+++ src/hackadash/screens/dashboard.py
@@ -6,6 +6,7 @@ import http.client
 import configparser
 from pathlib import Path
 import json
+from datetime import datetime, timedelta, timezone
 
 
 
@@ -19,6 +20,10 @@ class DashboardScreen(Screen):
     def compose(self) -> ComposeResult:
         yield Header()
         yield Label("Name: ...", id="displayName")
+        yield Label("ID: ...", id="userId")
+        yield Label("Total Time: ...", id="totalAllTime")
+        yield Label("Time past 7 days: ...", id="totalWeekTime")
+        yield Label("Daily avg past 7 days: ...", id="dailyAvgTime")
         yield Footer()
 
     def _on_mount(self, event):
@@ -45,19 +50,32 @@ class DashboardScreen(Screen):
 
             conn.request(
                 "GET",
-                "/api/hackatime/v1/users/%7Bid%7D?api_key=YOUR_SECRET_TOKEN",
+                "/api/v1/users/my/stats?start_date=&end_date=&limit=1&features=languages,projects&filter_by_project=&filter_by_category=&boundary_aware=true&total_seconds=false&no_ai_coding=true&test_param=true",
                 headers=headers
             )
+            response = conn.getresponse().read().decode("utf-8")
+            data_dict = json.loads(response)
+            self.display_name = data_dict["data"]["username"]
+            self.user_id = data_dict["data"]["user_id"]
+            self.total_seconds_readable = data_dict["data"]["human_readable_total"]
 
-            response = conn.getresponse()
-            raw_data = response.read().decode("utf-8")
-            data_dict = json.loads(raw_data)
+            conn.request(
+                "GET",
+                f"/api/v1/users/my/stats?start_date={(datetime.now(timezone.utc) - timedelta(days=7)).isoformat(timespec='seconds')}&end_date=&limit=1&features=languages,projects&filter_by_project=&filter_by_category=&boundary_aware=true&total_seconds=false&no_ai_coding=true&test_param=true",
+                headers=headers
+            )
+            response = conn.getresponse().read().decode("utf-8")
+            data_dict = json.loads(response)
+            self.total_week_readable = data_dict["data"]["human_readable_total"]
+            self.daily_avg_week_readable = data_dict["data"]["human_readable_daily_average"]
 
-            self.display_name = data_dict["data"]["display_name"]
 
             conn.close()
 
-
-            self.query_one("#displayName", Label).update("Name : " + self.display_name)
+            self.query_one("#displayName", Label).update(f"Name: {self.display_name}")
+            self.query_one("#userId", Label).update(f"ID: {self.user_id}")
+            self.query_one("#totalWeekTime", Label).update(f"Time past 7 days: {self.total_week_readable}")
+            self.query_one("#dailyAvgTime", Label).update(f"Daily avg past 7 days: {self.daily_avg_week_readable}")
+            self.query_one("#totalAllTime", Label).update(f"Total Time: {self.total_seconds_readable}")
         else:
             self.app.switch_screen("onboarding")
