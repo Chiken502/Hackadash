240f76d34556bd209fee37be7e515557fb174d66
Chicken502 <bcnelson100@gmail.com>
2026-09-07 14:45:42 -0400

    Implemented all needed APIs

--- src/hackadash/screens/dashboard.py
+++ src/hackadash/screens/dashboard.py
@@ -24,6 +24,8 @@ class DashboardScreen(Screen):
         yield Label("Total Time: ...", id="totalAllTime")
         yield Label("Time past 7 days: ...", id="totalWeekTime")
         yield Label("Daily avg past 7 days: ...", id="dailyAvgTime")
+        yield Label("Daily Leaderboard Rank: ...", id="dailyLeaderboardRank")
+        yield Label("Weekly Leaderboard Rank: ...", id="weeklyLeaderboardRank")
         yield Footer()
 
     def _on_mount(self, event):
@@ -31,7 +33,6 @@ class DashboardScreen(Screen):
 
     def action_refresh(self) -> None:
         self.fetch_data()
-
 
     def fetch_data(self):
         settings_path = Path("settings.cfg")
@@ -42,7 +43,7 @@ class DashboardScreen(Screen):
             api_key = config["API Key"]["api_key"]
             api_url = config["API Key"]["api_url"]
 
-            conn = http.client.HTTPSConnection("hackatime.hackclub.com")
+            conn = http.client.HTTPSConnection("hackatime.hackclub.com", timeout=30)
 
             headers = {
                 "Authorization": f"Bearer {api_key}"
@@ -58,6 +59,9 @@ class DashboardScreen(Screen):
             self.display_name = data_dict["data"]["username"]
             self.user_id = data_dict["data"]["user_id"]
             self.total_seconds_readable = data_dict["data"]["human_readable_total"]
+            self.streak = data_dict["data"]["streak"]
+            self.projects = data_dict["data"]["projects"]
+            self.languages = data_dict["data"]["languages"]
 
             conn.request(
                 "GET",
@@ -68,7 +72,38 @@ class DashboardScreen(Screen):
             data_dict = json.loads(response)
             self.total_week_readable = data_dict["data"]["human_readable_total"]
             self.daily_avg_week_readable = data_dict["data"]["human_readable_daily_average"]
+            self.projects_week = data_dict["data"]["projects"]
+            self.languages_week = data_dict["data"]["languages"]
 
+            conn.request(
+                "GET",
+                "/api/v1/leaderboard/daily",
+            )
+            response = conn.getresponse().read().decode("utf-8")
+            data_dict = json.loads(response)
+
+            self.daily_leaderboard_rank = -1
+            for i in data_dict["entries"]:
+                if str(i["user"]["id"]) == str(self.user_id):
+                    self.daily_leaderboard_rank = i["rank"]
+                    break
+
+            self.daily_leaderboard_last_update = data_dict["generated_at"]
+
+            conn.request(
+                "GET",
+                "/api/v1/leaderboard/weekly",
+            )
+            response = conn.getresponse().read().decode("utf-8")
+            data_dict = json.loads(response)
+
+            self.weekly_leaderboard_rank = -1
+            for i in data_dict["entries"]:
+                if str(i["user"]["id"]) == str(self.user_id):
+                    self.weekly_leaderboard_rank = i["rank"]
+                    break
+
+            self.weekly_leaderboard_last_update = data_dict["generated_at"]
 
             conn.close()
 
@@ -77,5 +112,7 @@ class DashboardScreen(Screen):
             self.query_one("#totalWeekTime", Label).update(f"Time past 7 days: {self.total_week_readable}")
             self.query_one("#dailyAvgTime", Label).update(f"Daily avg past 7 days: {self.daily_avg_week_readable}")
             self.query_one("#totalAllTime", Label).update(f"Total Time: {self.total_seconds_readable}")
+            self.query_one("#dailyLeaderboardRank", Label).update(f"Daily Leaderboard Rank: {self.daily_leaderboard_rank}")
+            self.query_one("#weeklyLeaderboardRank", Label).update(f"Weekly Leaderboard Rank: {self.weekly_leaderboard_rank}")
         else:
             self.app.switch_screen("onboarding")
