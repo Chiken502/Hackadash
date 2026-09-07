c8b0c27797103d9b5b2ff5acb8427291382a5565
Chicken502 <bcnelson100@gmail.com>
2026-09-07 15:25:34 -0400

    Split fetch data into multiple functions
    also added in loading text when you use the refresh action

--- src/hackadash/screens/dashboard.py
+++ src/hackadash/screens/dashboard.py
@@ -30,91 +30,158 @@ class DashboardScreen(Screen):
         yield Footer()
 
     def _on_mount(self, event):
-        self.fetch_data()
+        self.fetch_cfg_data()
+        self.fetch_user_data()
+
+        self.fetch_total_stats()
+        self.fetch_weekly_stats()
+        self.fetch_daily_leaderboard_data()
+        self.fetch_weekly_leaderboard_data()
 
     def action_refresh(self) -> None:
-        self.fetch_data()
+        self.query_one("#totalAllTime", Label).update("Total Time: ...")
+        self.query_one("#totalWeekTime", Label).update("Time past 7 days: ...")
+        self.query_one("#dailyAvgTime", Label).update("Daily avg past 7 days: ...")
+        self.query_one("#dailyLeaderboardRank", Label).update("Daily Leaderboard Rank: ...")
+        self.query_one("#weeklyLeaderboardRank", Label).update("Weekly Leaderboard Rank: ...")
 
-    @work(exclusive=True, thread=True)
-    async def fetch_data(self):
+
+        self.fetch_total_stats()
+        self.fetch_weekly_stats()
+        self.fetch_daily_leaderboard_data()
+        self.fetch_weekly_leaderboard_data()
+
+    def fetch_cfg_data(self):
         settings_path = Path("settings.cfg")
         if settings_path.is_file():
             config = configparser.ConfigParser()
             config.read("settings.cfg")
 
-            api_key = config["API Key"]["api_key"]
-            api_url = config["API Key"]["api_url"]
-
-            conn = http.client.HTTPSConnection("hackatime.hackclub.com", timeout=30)
-
-            headers = {
-                "Authorization": f"Bearer {api_key}"
-            }
-
-            conn.request(
-                "GET",
-                "/api/v1/users/my/stats?start_date=&end_date=&limit=1&features=languages,projects&filter_by_project=&filter_by_category=&boundary_aware=true&total_seconds=false&no_ai_coding=true&test_param=true",
-                headers=headers
-            )
-            response = conn.getresponse().read().decode("utf-8")
-            data_dict = json.loads(response)
-            self.display_name = data_dict["data"]["username"]
-            self.user_id = data_dict["data"]["user_id"]
-            self.total_seconds_readable = data_dict["data"]["human_readable_total"]
-            self.streak = data_dict["data"]["streak"]
-            self.projects = data_dict["data"]["projects"]
-            self.languages = data_dict["data"]["languages"]
-
-            conn.request(
-                "GET",
-                f"/api/v1/users/my/stats?start_date={(datetime.now(timezone.utc) - timedelta(days=7)).isoformat(timespec='seconds')}&end_date=&limit=1&features=languages,projects&filter_by_project=&filter_by_category=&boundary_aware=true&total_seconds=false&no_ai_coding=true&test_param=true",
-                headers=headers
-            )
-            response = conn.getresponse().read().decode("utf-8")
-            data_dict = json.loads(response)
-            self.total_week_readable = data_dict["data"]["human_readable_total"]
-            self.daily_avg_week_readable = data_dict["data"]["human_readable_daily_average"]
-            self.projects_week = data_dict["data"]["projects"]
-            self.languages_week = data_dict["data"]["languages"]
-
-            conn.request(
-                "GET",
-                "/api/v1/leaderboard/daily",
-            )
-            response = conn.getresponse().read().decode("utf-8")
-            data_dict = json.loads(response)
-
-            self.daily_leaderboard_rank = -1
-            for i in data_dict["entries"]:
-                if str(i["user"]["id"]) == str(self.user_id):
-                    self.daily_leaderboard_rank = i["rank"]
-                    break
-
-            self.daily_leaderboard_last_update = data_dict["generated_at"]
-
-            conn.request(
-                "GET",
-                "/api/v1/leaderboard/weekly",
-            )
-            response = conn.getresponse().read().decode("utf-8")
-            data_dict = json.loads(response)
-
-            self.weekly_leaderboard_rank = -1
-            for i in data_dict["entries"]:
-                if str(i["user"]["id"]) == str(self.user_id):
-                    self.weekly_leaderboard_rank = i["rank"]
-                    break
-
-            self.weekly_leaderboard_last_update = data_dict["generated_at"]
-
-            conn.close()
-
-            self.query_one("#displayName", Label).update(f"Name: {self.display_name}")
-            self.query_one("#userId", Label).update(f"ID: {self.user_id}")
-            self.query_one("#totalWeekTime", Label).update(f"Time past 7 days: {self.total_week_readable}")
-            self.query_one("#dailyAvgTime", Label).update(f"Daily avg past 7 days: {self.daily_avg_week_readable}")
-            self.query_one("#totalAllTime", Label).update(f"Total Time: {self.total_seconds_readable}")
-            self.query_one("#dailyLeaderboardRank", Label).update(f"Daily Leaderboard Rank: {self.daily_leaderboard_rank}")
-            self.query_one("#weeklyLeaderboardRank", Label).update(f"Weekly Leaderboard Rank: {self.weekly_leaderboard_rank}")
+            self.api_key = config["API Key"]["api_key"]
+            self.api_url = config["API Key"]["api_url"]
         else:
             self.app.switch_screen("onboarding")
+    
+    def fetch_user_data(self):
+        conn = http.client.HTTPSConnection("hackatime.hackclub.com", timeout=30)
+                
+        headers = {
+            "Authorization": f"Bearer {self.api_key}"
+        }
+
+        conn.request(
+            "GET",
+            "/api/hackatime/v1/users/current",
+            headers=headers
+        )
+
+        response = conn.getresponse().read().decode("utf-8")
+        data_dict = json.loads(response)
+
+        self.display_name = data_dict["data"]["display_name"]
+        self.user_id = data_dict["data"]["id"]
+        self.user_photo = data_dict["data"]["photo"]
+
+        self.query_one("#displayName", Label).update(f"Name: {self.display_name}")
+        self.query_one("#userId", Label).update(f"ID: {self.user_id}")
+
+        conn.close()
+    
+    @work(exclusive=True, thread=True)
+    async def fetch_daily_leaderboard_data(self):
+        conn = conn = http.client.HTTPSConnection("hackatime.hackclub.com", timeout=30)
+
+        conn.request(
+            "GET",
+            "/api/v1/leaderboard/daily",
+        )
+        response = conn.getresponse().read().decode("utf-8")
+        data_dict = json.loads(response)
+
+        self.daily_leaderboard_rank = -1
+        for i in data_dict["entries"]:
+            if str(i["user"]["id"]) == str(self.user_id):
+                self.daily_leaderboard_rank = i["rank"]
+                break
+
+        self.daily_leaderboard_last_update = data_dict["generated_at"]
+
+        self.query_one("#dailyLeaderboardRank", Label).update(f"Daily Leaderboard Rank: {self.daily_leaderboard_rank}")
+
+        conn.close()
+
+    @work(exclusive=True, thread=True)
+    async def fetch_weekly_leaderboard_data(self):
+        conn = conn = http.client.HTTPSConnection("hackatime.hackclub.com", timeout=30)
+
+        conn.request(
+            "GET",
+            "/api/v1/leaderboard/weekly",
+        )
+        response = conn.getresponse().read().decode("utf-8")
+        data_dict = json.loads(response)
+
+        self.weekly_leaderboard_rank = -1
+        for i in data_dict["entries"]:
+            if str(i["user"]["id"]) == str(self.user_id):
+                self.weekly_leaderboard_rank = i["rank"]
+                break
+
+        self.weekly_leaderboard_last_update = data_dict["generated_at"]
+
+        self.query_one("#weeklyLeaderboardRank", Label).update(f"Weekly Leaderboard Rank: {self.weekly_leaderboard_rank}")
+
+        conn.close()
+
+    @work(exclusive=True, thread=True)
+    async def fetch_weekly_stats(self):
+        conn = http.client.HTTPSConnection("hackatime.hackclub.com", timeout=30)
+        
+        headers = {
+            "Authorization": f"Bearer {self.api_key}"
+        }
+
+        conn.request(
+            "GET",
+            f"/api/v1/users/my/stats?start_date={(datetime.now(timezone.utc) - timedelta(days=7)).isoformat(timespec='seconds')}&end_date=&limit=1&features=languages,projects&filter_by_project=&filter_by_category=&boundary_aware=true&total_seconds=false&no_ai_coding=true&test_param=true",
+            headers=headers
+        )
+
+        response = conn.getresponse().read().decode("utf-8")
+        data_dict = json.loads(response)
+
+        self.total_week_readable = data_dict["data"]["human_readable_total"]
+        self.daily_avg_week_readable = data_dict["data"]["human_readable_daily_average"]
+        self.projects_week = data_dict["data"]["projects"]
+        self.languages_week = data_dict["data"]["languages"]
+
+        self.query_one("#totalWeekTime", Label).update(f"Time past 7 days: {self.total_week_readable}")
+        self.query_one("#dailyAvgTime", Label).update(f"Daily avg past 7 days: {self.daily_avg_week_readable}")
+
+        conn.close()
+
+    @work(exclusive=True, thread=True)
+    async def fetch_total_stats(self):
+        conn = http.client.HTTPSConnection("hackatime.hackclub.com", timeout=30)
+        
+        headers = {
+            "Authorization": f"Bearer {self.api_key}"
+        }
+
+        conn.request(
+            "GET",
+            "/api/v1/users/my/stats?start_date=&end_date=&limit=1&features=languages,projects&filter_by_project=&filter_by_category=&boundary_aware=true&total_seconds=false&no_ai_coding=true&test_param=true",
+            headers=headers
+        )
+
+        response = conn.getresponse().read().decode("utf-8")
+        data_dict = json.loads(response)
+
+        self.total_seconds_readable = data_dict["data"]["human_readable_total"]
+        self.streak = data_dict["data"]["streak"]
+        self.projects = data_dict["data"]["projects"]
+        self.languages = data_dict["data"]["languages"]
+
+        self.query_one("#totalAllTime", Label).update(f"Total Time: {self.total_seconds_readable}")
+
+        conn.close()
